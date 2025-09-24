945d0daa02a027aea9630daaf11efd18192670d0
OkapiGG <alancervantes800@gmail.com>
2025-09-24 11:08:22 -0600

    loginPrueba

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,52 +1,73 @@
 import styled from "styled-components/native";
 
-export default function LoginScreen(){
-    // const dataRutas = [
-    //     {name: "proyConversor", href:"/(proyConversor)"},
-    //     {name: "teoria", href:"/(teoria)"}
-    // ];
-
-    // const renderItem = ({item})=>{
-    //     return(
-    //         <Link href={item.href}>
-    //             <Title>{item.name}</Title>
-    //         </Link>
-    //     )
-    // };
-
-    return(
+export default function LoginScreen() {
+    return (
         <Container>
-            <Title>Iniciar Sesión</Title>
+            <Title>Bienvenido</Title>
 
-            <StyledInput placeholder="Correo electrónico"/>
+            <StyledInput 
+                placeholder="Correo electrónico"
+                placeholderTextColor="#999"
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
 
-            <StyledInput placeholder="Contraseña"/>
+            <StyledInput 
+                placeholder="Contraseña"
+                placeholderTextColor="#999"
+                secureTextEntry
+            />
 
-            <LoginButton>
+            <LoginButton activeOpacity={0.8}>
                 <ButtonText>Ingresar</ButtonText>
             </LoginButton>
         </Container>
-    )
+    );
 }
 
-const Container = styled.View`
-    flex:1;
-    justify-content:center;
-    align-items:center;
-    background-color: #f7e7ce;
-`;
-const Title = styled.Text`
 
+
+// ---Estilos---
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    background-color: #F7F7F7; 
+    padding: 25px;
 `;
+
+const Title = styled.Text`
+    font-size: 36px;
+    font-weight: bold; 
+    color: #212121;
+    margin-bottom: 30px;
+    text-align: center;
+`;
+
 const StyledInput = styled.TextInput`
-    background-color: white;
-    border: 2px solid black;
-    padding: 5px;
-    margin-top: 10px;
+    width: 100%;
+    height: 55px;
+    background-color: #FFFFFF;
+    border: 1px solid #E0E0E0;
+    border-radius: 12px;
+    padding: 15px;
+    margin-top: 15px;
+    font-size: 16px;
+    color: #333;
 `;
-const LoginButton = styled.Text`
-    
+
+const LoginButton = styled.TouchableOpacity` 
+    width: 100%;
+    padding: 18px;
+    margin-top: 25px;
+    background-color: #007AFF;
+    border-radius: 12px;
+    align-items: center;
+    box-shadow: 0px 5px 15px rgba(0, 122, 255, 0.3);
+    elevation: 5;
 `;
+
 const ButtonText = styled.Text`
-    
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
 `;
